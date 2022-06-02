87e5a797fa1c8baf8db8a36624a2f35a6ff91974
es5es5 <es5e5s@nate.com>
2022-06-02 16:02:23 +0900

    added Swagger

--- src/photo/entities/photo.entity.ts
+++ src/photo/entities/photo.entity.ts
@@ -1,22 +1,29 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('photo')
 export class Photo {
   @PrimaryGeneratedColumn()
+  @ApiProperty()
   id: number;
 
   @Column({ length: 500 })
+  @ApiProperty()
   name: string;
 
   @Column('text')
+  @ApiProperty()
   description: string;
 
   @Column()
+  @ApiProperty()
   filename: string;
 
   @Column('int')
+  @ApiProperty()
   views: number;
 
   @Column()
+  @ApiProperty()
   isPublished: boolean;
 }
